8db25e757c52a21ab9f331b0375f239bcba77bfc
Unknown <adrian.gonzalez@bluewin.ch>
2018-05-08 11:23:01 +0200

    Update runApp
    The script has been updated with the checking of the windows operating system version.

--- src/runApp.m
+++ src/runApp.m
@@ -26,12 +26,25 @@ Folders= {'UI';
     'BruteForce';
     };
 
-%Check if the user's computer is a pc or mac/linux
+%Check if the user's computer is a pc
 if ispc
-    %TODO: get Windows version and prompt a warning if its not Windows 10.
-    system_dependent('getwinsys');
+    
+    %get the current Windows-version, extract the version from the string
+    %and typecast it to double for comparsion
+    Version= char(system_dependent('getwinsys'));
+    Version= char(regexp(Version,'Version 1\d.\d*','match'));
+    Version= str2double(char(regexp(Version,'\d\d.\d','match')));
+    
+    %Compare if the version of Windows is before 10.0 and prompt a warning
+    %if its before 10.0
+    if Version < 10.0
+        uiwait(msgbox(sprintf(['Your operating system is not "Windows 10"'...
+            ,'or higher.','\n','This application may not work properly.']),...
+            'Operating System Version Warning','warn'));
+    end
     Slash= '\';
-elseif isunix
+    
+else %isunix or unknown
     %UI to inform about not supported operating systems.
     uiwait(msgbox(['This application may not work properly because your ',...
         'operating system is not "Windows".'],'Operating System Warning','warn'));
@@ -40,7 +53,7 @@ end
 
 %Check if the user is a student and prompt a warning if not.
 if ~isstudent
-    uiwait(msgbox('This application should only be used for education!',...
+    uiwait(msgbox('This application should be used for education only!',...
         'Operating System Warning','warn'));
 end
 
@@ -52,3 +65,5 @@ end
 %==============================Calling the GUI=============================
 App= userInterface_script;
 %==========================================================================
+
+%TODO: Appkill function like rmpath and rm Vars
