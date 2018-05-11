c492f12f4fb2d4256cdfca144df8bd8bfbb52fbd
A. Gonzalez <adrian.gonzalez@bluewin.ch>
2018-05-11 14:58:55 +0200

    Update on function getCpuData
    The path to the file is now dynamic per user.

--- src/getCpuData.m
+++ src/getCpuData.m
@@ -21,12 +21,23 @@
 %Version:           1.0
 
 function info = getCpuData
-[~,cpuTermalZones] = system('powershell -inputformat none -file C:\DATA\FHNW\8_Semester\matl\matlProject\src\Scripts\get-temperature-TZ0.ps1');
+
+%Define slash for operating system
+if ispc
+    Slash= '\';
+else
+    Slash= '/';
+end
+
+%define the path to the powershell scripts per each user
+UserPath= [pwd,Slash,'Scripts',Slash];
+
+[~,cpuTermalZones] = system(sprintf('powershell -inputformat none -file %sget-temperature-TZ0.ps1',UserPath));
 cpuTermalZones = regexp(cpuTermalZones,'\d*','match');
 cpuTempKelvin = str2double(horzcat(cpuTermalZones{:}));
 dblTempCelsius = (cpuTempKelvin / 10) - 273.15;
 info.currCpuTemp = {num2str(dblTempCelsius)};
 
-[~,cpuAvgLoad] = system('powershell -inputformat none -file C:\DATA\FHNW\8_Semester\matl\matlProject\src\Scripts\get-cpu-load.ps1');
+[~,cpuAvgLoad] = system(sprintf('powershell -inputformat none -file %sget-cpu-load.ps1',UserPath));
 info.avgCpuLoad = regexp(cpuAvgLoad,'\d*','match');
 return 
