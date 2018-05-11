b1507a83024620723febbe551d0e9008ac7d2075
Bruno Hürzeler <bruno.huerzeler@students.fhnw.ch>
2018-05-11 17:11:50 +0200

    New function getGpuData added
    - PowerShell script for gpu temperature edited

--- src/Scripts/get-gpu-temperature.ps1
+++ src/Scripts/get-gpu-temperature.ps1
@@ -1,17 +1,1 @@
-$computer = $env:COMPUTERNAME
-$namespace = "ROOT\cimv2\NV"
-$gpuclass = "Gpu"
-$tempclass = "ThermalProbe"
-
-$GPU_Meters=Get-WmiObject -Class $gpuclass -ComputerName $computer -Namespace $namespace | Select-Object *
-$TEMP_Meters=Get-WmiObject -Class $tempclass -ComputerName $computer -Namespace $namespace | Select-Object *
-$nvidia_model=$GPU_Meters.productName
-$gpu_usage=$GPU_Meters.percentGpuUsage
-$mem_usage=$GPU_Meters.percentGpuMemoryUsage
-$temperature=$TEMP_Meters.temperature
-
-$output = "Nvidia $nvidia_model Memory:$mem_usage% GPU:$gpu_usage% Temp:$Temperature°C | Memory=$mem_usage;;;0; GPU=$gpu_usage;;;0; TEMP=$temperature;;;0;"
-
-Write-Output $Output
-
-exit 0
+Get-WmiObject -Class Win32_PerfFormattedData_Counters_ThermalZoneInformation | where-object {$_.Name -eq "\_SB.PCI0.LPCB.TZ05"} | Select-Object Temperature
---
+++ src/getGpuData.m
@@ -0,0 +1,44 @@
+%**************************************************************************
+%Project:           Brute-Force Tool
+%
+%Authors:           B. Hürzeler
+%                   A. Gonzalez
+%
+%Name:              getGpuData
+%
+%Description:       TODO
+%
+%Input:             No input
+%
+%Output:            No output
+%
+%Example:           getGpuData();
+%
+%Copyright:
+%
+%**************************************************************************
+
+%Version:           1.0
+
+function info = getGpuData
+
+%Define slash for operating system
+if ispc
+    Slash= '\';
+else
+    Slash= '/';
+end
+
+%define the path to the powershell scripts per each user
+UserPath= [pwd,Slash,'Scripts',Slash];
+
+%TODO: choose powershell scripts per GUI or dynamically
+[~,gpuTermalZones] = system(sprintf('powershell -inputformat none -file %sget-gpu-temperature.ps1',UserPath));
+gpuTermalZones = regexp(gpuTermalZones,'\d*','match');
+gpuTempKelvin = str2double(horzcat(gpuTermalZones{:}));
+dblTempCelsius = gpuTempKelvin - 273.15;
+info.currGpuTemp = {num2str(dblTempCelsius)};
+
+% [~,cpuAvgLoad] = system(sprintf('powershell -inputformat none -file %sget-cpu-load.ps1',UserPath));
+% info.avgCpuLoad = regexp(cpuAvgLoad,'\d*','match');
+return 
