372cdc404e582dafb26e90e708ca2e3f74c89043
Bruno Hürzeler <bruno.huerzeler@students.fhnw.ch>
2018-05-12 16:01:01 +0200

    displayData edited version 1.0

--- src/displayData.m
+++ src/displayData.m
@@ -1,22 +1,70 @@
-function info = displayData
-time = 1;
+%**************************************************************************
+%Project:           Brute-Force Tool
+%
+%Authors:           B. Hürzeler
+%                   A. Gonzalez
+%
+%Name:              displayData
+%
+%Description:       TODO
+%
+%Input:             Object Obj of the class userInterface
+%
+%Output:            Object Obj of the class userInterface
+%
+%Example:           displayData;
+%
+%Copyright:
+%
+%**************************************************************************
+
+%==========================================================================
+%<Version 1.0> - 12.05.2018 - First version of the function.
+%==========================================================================
+
+function [Obj] = displayData(Obj)
+%FIXME: prevTime should be declarated in public in app object
+prevTime = 0;
+time = 1 + prevTime;
 value = 0;
 
 
-obj= figure;
-plot(time ,value);
-obj.Children.YAxis.Limits = [0 100];
-obj.Children.XAxis.Limits = [0 60];
-obj.Children.YAxis.LimitsMode = 'manual';
-obj.Children.XAxis.Direction = 'reverse';
+% fig= figure;
+% plot(time ,value,'LineWidth',1.5,'Color','red');
 
-for index = 1:60
-    value(end+1) = index^1.5;
-    time(end+1) = time(end)+1;
-    obj.Children.Children.YData= value;
-    obj.Children.Children.XData= time;
-    pause(0.5)
+area(value, 'LineWidth', 1,...
+    'FaceColor', 'red',...
+    'FaceAlpha', 0.7,...
+    'AlignVertexCenters', 'on');
+
+value(end+1) = Obj.cpuData.avgCpuLoad;
+time(end+1) = time(end)+1;
+   
+%Write data in axis object
+Obj.UIAxes_cpu.YData = value;
+Obj.UIAxes_cpu.XData = time;
+
+%Set old value
+prevTime = time(end+1);
+
+%Set Timer to first element
+if time(end) == 61
+    prevTimer = 0;
 end
+
+
+% fig.Children.YAxis.Limits = [0 100];
+% fig.Children.XAxis.Limits = [0 60];
+% fig.Children.YAxis.LimitsMode = 'manual';
+% fig.Children.XAxis.Direction = 'reverse';
+
+% for index = 1:60
+%     value(end+1) = index^1.5;
+%     time(end+1) = time(end)+1;
+%     fig.Children.Children.YData= value;
+%     fig.Children.Children.XData= time;
+%     pause(0.5)
+% end
 
 
 
