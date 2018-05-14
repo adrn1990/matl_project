625507949fe656871d08f7669da58d5cb22270d1
A. Gonzalez <adrian.gonzalez@bluewin.ch>
2018-05-14 19:17:39 +0200

    Update function displayData

--- src/displayData.m
+++ src/displayData.m
@@ -30,24 +30,17 @@ CpuData = getCpuData;
 % Obj.CpuTemperatureOutput.Value = Obj.CpuData.currCpuTemp;
 
 %Get GPU data
-% GpuData = getGpuData;
+GpuData = getGpuData;
 % Obj.GpuLoadOutput.Value = Obj.GpuData.avgGpuLoad;
 % Obj.GpuTemperatureOutput.Value = Obj.GpuData.currGpuTemp;
 
-%FIXME: area() function does not work
-% area(Obj.UIAxes_cpu.Children.YData, 'LineWidth', 1,...
-%     'FaceColor', 'red',...
-%     'FaceAlpha', 0.7,...
-%     'AlignVertexCenters', 'on');
-
+Obj.CpuValue(1) = str2double(CpuData.avgCpuLoad);
+Obj.GpuValue(1) = str2double(GpuData.avgGpuLoad);
+    
 %Convert values into double and write in array
-if ~Obj.SizeReached
-    Obj.CpuValue(end) = str2double(CpuData.avgCpuLoad);
-    %Obj.UIAxes_cpu.Children.YData = area(Obj.UIAxes_cpu.Children.YData);
-    % Obj.GpuValue(end+1) = str2double(GpuData.avgGpuLoad);
-else
+if Obj.SizeReached
     Obj.CpuValue = circshift(Obj.CpuValue,1);
-    Obj.CpuValue(1) = str2double(CpuData.avgCpuLoad);
+    Obj.GpuValue = circshift(Obj.GpuValue,1);
 end
    
 %Write data in CPUaxis object
@@ -55,17 +48,18 @@ Obj.UIAxes_cpu.Children.YData = Obj.CpuValue;
 Obj.UIAxes_cpu.Children.XData= Obj.time;
 
 %Write data in GPUaxis object
-% Obj.UIAxes_gpu.Children.YData = Obj.GpuValue;
-% Obj.UIAxes_gpu.Children.XData= Obj.time;
+Obj.UIAxes_gpu.Children.YData = Obj.GpuValue;
+Obj.UIAxes_gpu.Children.XData= Obj.time;
 
 %Prepare next index
 Obj.time(end+1) = Obj.time(end)+1;
-Obj.CpuValue(end+1) = 0;
+Obj.CpuValue = [0,Obj.CpuValue];
+Obj.GpuValue = [0,Obj.GpuValue];
 %Set Timer to first element
 if Obj.time(end) == 61
     Obj.SizeReached = true;
     Obj.time(1) = 0; 
-    Obj.CpuValue(1) = 0;
+    %Obj.CpuValue(1) = 0;
 end
 
 end
