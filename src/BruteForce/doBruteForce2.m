d07b33275d9c41c9d26ec3463f7bf748557cba62
A. Gonzalez <adrian.gonzalez@bluewin.ch>
2018-05-17 11:46:37 +0200

    Update on function doBruteForce 2
    implemented in UI, exception handling, improvement handling.

--- src/BruteForce/doBruteForce2.m
+++ src/BruteForce/doBruteForce2.m
@@ -44,53 +44,103 @@ Opt= Obj.HashStruct;
 % D = parallel.pool.DataQueue;
 % D.afterEach(@disp);
 
-for Increment=1:Length
-    if strcmp(Hash,Improvements{Increment,2}) && ...
-            strcmp(Method,Improvements{Increment,3})
-        FoundByImprovement= true;
-    end
-end
-
-
-%The parallel job will only be created if the password couldn't be found by
-%improvements.
-if ~FoundByImprovement
+Obj.fWriteMessageBuffer(Obj.delemiter);
+Obj.fWriteMessageBuffer('BruteForcing in progress...');
+Obj.fWriteMessageBuffer(sprintf('Started on %s',datestr(now,'dd.mm.yyyy at HH:MM:SS')));
+tic
+try
     
-    %TODO choose cluster
-    c = parcluster;
-    Job = createJob(c);
-    
-    %TODO: divide the task to more than two workers (if there are any)
-    Task1= createTask(Job, @doBruteForceAscendingly, 1, {Iterations,Hash,Array,Opt},'CaptureDiary',true);
-    Task2= createTask(Job, @doBruteForceRandomly, 1, {Iterations,Hash,Array,Opt},'CaptureDiary',true);
-    
-    %submit the job to the scheduler
-    submit(Job)
-    
-    Break= false;
-    
-    while(~strcmp(Task1.State,'finished') && ~strcmp(Task2.State,'finished') && ~Break)
-        %TODO: displayData
-        pause(3);
-        if Obj.Abort
-            %Abort the BruteForce by cancelling the job and break the while
-            %loop
-            Break = true;
-            Job.cancel;
+    %go first thru the improvements
+    for Increment=1:Length
+        if strcmp(Hash,Improvements{Increment,2}) && ...
+                strcmp(Method,Improvements{Increment,3})
+            FoundByImprovement= true;
+            Pw= {Improvements{Increment,1}};
+            break
         end
     end
     
-    %cancel the task which is not finished yet
-    if strcmp(Task1.State,'finished')
-        Task2.delete
-    elseif strcmp(Task2.State,'finished')
-        Task1.delete
+    %The parallel job will only be created if the password couldn't be found by
+    %improvements.
+    if ~FoundByImprovement
+        
+        %TODO: Divide Iterations for amount of workers and gpus
+        
+        
+        %TODO: Use of gpu
+        %     if UseGpu
+        %         G = gpuArray(Iterations);
+        %         doBruteForceAscendingly(G,Hash,Array,Opt);
+        %         doBruteForceRandomly(G,Hash,Array,Opt);
+        %     end
+        
+        %TODO choose cluster from UI
+        c = parcluster;
+        Job = createJob(c);
+        
+        %TODO: divide the task to more than two workers (if there are any)
+        Task1= createTask(Job, @doBruteForceAscendingly, 1, {Iterations,Hash,Array,Opt},'CaptureDiary',true);
+        Task2= createTask(Job, @doBruteForceRandomly, 1, {Iterations,Hash,Array,Opt},'CaptureDiary',true);
+        
+        %submit the job to the scheduler
+        submit(Job)
+        
+        Break= false;
+        
+        while(~strcmp(Task1.State,'finished') && ~strcmp(Task2.State,'finished') && ~Break)
+            %TODO: displaydata throws an exception
+            %displayData(Obj)
+            pause(3);
+            if Obj.Abort
+                %Abort the BruteForce by cancelling the job and break the while
+                %loop
+                Break = true;
+                Job.cancel;
+            end
+        end
+        
+        %TODO append this part for multiple workers / tasks
+        %cancel the task which is not finished yet
+        if strcmp(Task1.State,'finished')
+            Task2.delete
+        elseif strcmp(Task2.State,'finished')
+            Task1.delete
+        end
+        
+        %TODO: get data from gpu
+        %gather(G2);
+        Pw = fetchOutputs(Job);
+        Pw{1};
+        
     end
     
-    results = fetchOutputs(Job);
+    %if the password was found by improvement, save it into the variable
+    %imrovements
+    if ~FoundByImprovement
+        Improvements{end+1,1}= Pw{1};
+        Improvements{end,2}= DataHash(Pw{1},Opt);
+        Improvements{end,3}= Method;
+        Obj.Improvements= Improvements;
+    end
     
+    Obj.ResultOutput.Value= Pw;
+    
+catch ME
+    Obj.fWriteMessageBuffer('An error with the following message occured:');
+    Obj.fWriteMessageBuffer(ME.message);
 end
 
-%Handover Password
+%write the message buffer
+if (~isempty(Obj.ResultOutput.Value{1}))
+    Obj.fWriteStatus('Your current progress in BruteForcing is: 100%');
+    Obj.fWriteMessageBuffer('The BruteForcing was successfull!');
+    Obj.fWriteMessageBuffer(sprintf('Your Password is: %s',Obj.ResultOutput.Value{1}));
+    Obj.fWriteMessageBuffer(sprintf('Elapsed time is %f seconds',toc));
+    Obj.fWriteMessageBuffer(Obj.delemiter);
+else
+    Obj.fWriteStatus('Your current progress in BruteForcing is: 0%');
+    Obj.fWriteMessageBuffer('The BruteForcing was unfortunately not successfull!');
+    Obj.fWriteMessageBuffer('Please try again.');
+    Obj.fWriteMessageBuffer(Obj.delemiter);
 end
 
